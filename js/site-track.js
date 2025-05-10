2faab507ea30057010fad377df033c848d88dcee
Yuliia Rul <lhest@Yuliias-MacBook-Pro.local>
2025-05-10 23:52:48 +0200

    ac site tracker added

---
+++ js/site-track.js
@@ -0,0 +1,34 @@
+// This file is part of the ActiveCampaign Forms JavaScript API.
+    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+    vgo('setAccount', '92857244');
+    vgo('setTrackByDefault', true);
+
+    vgo('process');
+    /**
+     * Initializes the visitor tracking script and sets the account and default tracking behavior.
+     *
+     * @param {Window} e - The global window object.
+     * @param {Document} t - The document object.
+     * @param {string} o - The URL of the visitor tracking script.
+     * @param {string} n - The alias for the visitor tracking global object.
+     * @param {any} p - Unused parameter.
+     * @param {any} r - Unused parameter.
+     * @param {any} i - Unused parameter.
+     *
+     * @returns {void}
+     */
+    (function(e,t,o,n,p,r,i){
+        e.visitorGlobalObjectAlias=n;
+        e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};
+        e[e.visitorGlobalObjectAlias].l=(new Date).getTime();
+        r=t.createElement("script");
+        r.src=o;
+        r.async=true;
+        i=t.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(r,i);
+    })(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+    
+    vgo('setAccount', '92857244');
+    vgo('setTrackByDefault', true);
+    
+    vgo('process');
